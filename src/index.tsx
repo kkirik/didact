c0fd909ee2e5d9b5599e4e65a2b424c1926e9266
KriK96 <skuf962@yandex.ru>
2019-12-01 11:43:50 +0300

    Added Concurrent mode

---
+++ src/index.tsx
@@ -0,0 +1,14 @@
+/** @jsx Didact.createElement */
+import Didact, { IDidactElement } from "../packages/didact";
+
+const element: IDidactElement = (
+  <div style="background: salmon">
+    <h1>Hello World</h1>
+    <h2 style="text-align:right">from Didact</h2>
+    <h3 style="text-align:right">from Didact3</h3>
+  </div>
+);
+
+const container = document.querySelector(".root");
+
+Didact.render(element, container);
