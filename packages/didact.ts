1769038b172cdb96ecedcd40c56b11a1696c0108
KriK96 <skuf962@yandex.ru>
2020-05-17 17:46:20 +0300

    Didact fifth step

--- packages/didact.ts
+++ packages/didact.ts
@@ -7,6 +7,19 @@ export interface IDidactElement {
   };
 }
 
+export interface IFiberNode {
+  dom: Text | HTMLElement | Element;
+  parent?: IFiberNode;
+  child?: IFiberNode;
+  sibling?: IFiberNode;
+  type?: string;
+  props: {
+    [key: string]: any;
+    children: IDidactElement[];
+    nodeValue?: string;
+  };
+}
+
 function createElement(
   type: string,
   props?: { [key: string]: any },
@@ -33,25 +46,32 @@ function createTextElement(text: string): IDidactElement {
   };
 }
 
-function render(element: IDidactElement, container: Text | HTMLElement | Element) {
+function createDom(fiber: IFiberNode) {
   const dom =
-    element.type === 'TEXT_ELEMENT'
+    fiber.type === 'TEXT_ELEMENT'
       ? document.createTextNode('')
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
 
   const isChildren = (prop) => prop !== 'children';
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isChildren)
     .forEach((prop) => {
-      dom[prop] = element.props[prop];
+      dom[prop] = fiber.props[prop];
     });
 
-  element.props.children.forEach((child) => render(child, dom));
-
-  container.appendChild(dom);
+  return dom;
 }
 
-let nextUnitOfWork = null;
+function render(element: IDidactElement, container: Text | HTMLElement | Element) {
+  nextUnitOfWork = {
+    dom: container,
+    props: {
+      children: [element],
+    },
+  };
+}
+
+let nextUnitOfWork: IFiberNode | null = null;
 
 function workLoop(deadline: IdleDeadline) {
   let shouldYield = false;
@@ -60,10 +80,63 @@ function workLoop(deadline: IdleDeadline) {
     nextUnitOfWork = performUnitOfWork(nextUnitOfWork);
     shouldYield = deadline.timeRemaining() < 1;
   }
+
+  requestIdleCallback(workLoop);
 }
 
-function performUnitOfWork(nextUnitOfWork) {
-  // TODO
+function performUnitOfWork(fiber: IFiberNode) {
+  // TODO add dom node
+  // TODO create new fibers
+  // TODO return next unit of work
+
+  // TODO add dom node
+  if (!fiber.dom) {
+    fiber.dom = createDom(fiber);
+  }
+
+  if (fiber.parent) {
+    fiber.parent.dom.appendChild(fiber.dom);
+  }
+
+  // TODO create new fibers
+  const elements = fiber.props.children;
+  let index = 0;
+  let prevSibling: IFiberNode = null;
+
+  while (index < elements.length) {
+    const element = elements[index];
+
+    const newFiber: IFiberNode = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: null,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+
+  // TODO return next unit of work
+  if (fiber.child) {
+    return fiber.child;
+  }
+
+  let nextFiber = fiber;
+
+  while (nextFiber) {
+    if (nextFiber.sibling) {
+      return nextFiber.sibling;
+    }
+
+    nextFiber = nextFiber.parent;
+  }
 }
 
 requestIdleCallback(workLoop);
