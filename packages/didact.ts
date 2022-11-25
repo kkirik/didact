76c7cc64f0eb39486ee692c377ca3460fd1968e7
kirkharitonov <kirkharitonov@yandex-team.ru>
2022-11-25 22:56:51 +0100

    created new fibers

--- packages/didact.ts
+++ packages/didact.ts
@@ -11,16 +11,26 @@ export interface DidactElement {
   };
 }
 
-export interface UnitOfWork {
-  // type?: string;
+interface RootUnitOfWork {
   dom?: Element | Text;
-  parent?: UnitOfWork;
-  child?: UnitOfWork;
-  sibling?: UnitOfWork;
+  child?: FiberNode;
   props: {
-    children: UnitOfWork[];
+    children: FiberNode[];
   };
 }
+
+interface FiberNode {
+  type: keyof HTMLElementTagNameMap | 'TEXT_ELEMENT';
+  dom?: Element | Text;
+  parent?: UnitOfWork;
+  child?: FiberNode;
+  sibling?: FiberNode;
+  props: {
+    children: FiberNode[];
+  };
+}
+
+export type UnitOfWork = RootUnitOfWork | FiberNode;
 
 function createTextElement(text: string): DidactElement {
   return {
@@ -48,22 +58,22 @@ function createElement(
   };
 }
 
-function createDomNode(element: DidactElement) {
+function createDomNode(fiber: FiberNode) {
   const dom =
-    element.type === 'TEXT_ELEMENT'
+    fiber.type === 'TEXT_ELEMENT'
       ? document.createTextNode('')
-      : document.createElement(element.type);
+      : document.createElement(fiber.type);
 
-  element.props.children.forEach((child) => {
+  fiber.props.children.forEach((child) => {
     render(child, dom);
   });
 
   const isNodeProperty = (key) => key !== 'children';
 
-  Object.keys(element.props)
+  Object.keys(fiber.props)
     .filter(isNodeProperty)
     .forEach((name) => {
-      dom[name] = element.props[name];
+      dom[name] = fiber.props[name];
     });
 
   return dom;
@@ -71,7 +81,7 @@ function createDomNode(element: DidactElement) {
 
 let nextUnitOfWork: UnitOfWork | null = null;
 
-function render(element: UnitOfWork, container: Element | Text) {
+function render(element: FiberNode, container: Element | Text) {
   nextUnitOfWork = {
     dom: container,
     props: {
@@ -95,15 +105,40 @@ function workLoop(deadline: IdleDeadline) {
 requestIdleCallback(workLoop);
 
 function performUnitOfWork(fiber: UnitOfWork): UnitOfWork | null {
-  if (!fiber.dom) {
+  /** isn't root fiber node */
+  if ('type' in fiber && !fiber.dom) {
     fiber.dom = createDomNode(fiber);
   }
 
-  if (fiber.parent?.dom) {
+  if ('parent' in fiber && fiber.parent?.dom && fiber.dom) {
     fiber.parent.dom.appendChild(fiber.dom);
   }
 
-  // TODO create new fibers
+  const elements = fiber.props.children;
+
+  let index = 0;
+  let prevSibling: FiberNode | undefined = undefined;
+
+  while (index < elements.length) {
+    const element = elements[index];
+
+    const newFiber: FiberNode = {
+      type: element.type,
+      props: element.props,
+      parent: fiber,
+      dom: undefined,
+    };
+
+    if (index === 0) {
+      fiber.child = newFiber;
+    } else if (prevSibling) {
+      prevSibling.sibling = newFiber;
+    }
+
+    prevSibling = newFiber;
+    index++;
+  }
+
   // TODO return next unit of work
 
   return null;
