53881f5bbea7d52d542417e5614e7f88c3cb7f09
kirkharitonov <kirkharitonov@yandex-team.ru>
2022-11-26 12:42:28 +0100

    added example for rerender

--- src/index.tsx
+++ src/index.tsx
@@ -35,8 +35,21 @@ const newReactElement = (
 
 const container = document.querySelector('.root')!;
 
-Didact.render(element, container);
-// Didact.render(newElement, container);
+const updateValue = (e) => {
+  rerender(e.target.value);
+};
+
+const rerender = (value) => {
+  const element = (
+    <div>
+      <input onInput={updateValue} value={value} />
+      <h2>Hello {value}</h2>
+    </div>
+  );
+  Didact.render(element, container);
+};
+
+rerender('World');
 
 // react
 
